ed5545c7ca56fe35b7e80ee7ee60dc07487bbcbb
AriesNation <bradbotteron13@gmail.com>
2019-11-18 20:32:48 -0500

    added content to CrimeLab.java (singleton)

--- app/src/main/java/edu/ivytech/crimetrackerdemo/CrimeLab.java
+++ app/src/main/java/edu/ivytech/crimetrackerdemo/CrimeLab.java
@@ -1,4 +1,26 @@
 package edu.ivytech.crimetrackerdemo;
 
+import android.content.Context;
+
+/* !!!!!For recycler view!!!!! */
+
 public class CrimeLab {
+
+    public static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context)
+    {
+        if(sCrimeLab == null)
+        {
+            sCrimeLab = new CrimeLab(context);
+        }
+
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context)
+    {
+
+    }
+
 }
