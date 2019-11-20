bef1ef769dd179b6f37d9e48eefd228daa508d65
AriesNation <bradbotteron13@gmail.com>
2019-11-19 19:24:41 -0500

    changed CrimeActivity to extend SingleFragmentActivity

--- app/src/main/java/edu/ivytech/crimetrackerdemo/CrimeActivity.java
+++ app/src/main/java/edu/ivytech/crimetrackerdemo/CrimeActivity.java
@@ -1,27 +1,14 @@
 package edu.ivytech.crimetrackerdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-
-
-        FragmentManager fragman = getSupportFragmentManager();
-        Fragment fragment = fragman.findFragmentById(R.id.fragment_container);
-
-        if(fragment == null)
-        {
-            fragment = new CrimeFragment();
-            fragman.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment()
+    {
+        return new CrimeFragment();
     }
+
+    
 }
--- app/src/main/java/edu/ivytech/crimetrackerdemo/SingleFragmentActivity.java
+++ app/src/main/java/edu/ivytech/crimetrackerdemo/SingleFragmentActivity.java
@@ -1,6 +1,28 @@
 package edu.ivytech.crimetrackerdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-public abstract class SingleFragmentActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity
+{
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragman = getSupportFragmentManager();
+        Fragment fragment = fragman.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null)
+        {
+            fragment = new CrimeFragment();
+            fragman.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
 }
